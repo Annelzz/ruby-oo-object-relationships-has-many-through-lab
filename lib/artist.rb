8c3830aba3c2fa5a8d4b13b9dede3e02d907035c
Annelise <annelzz26@gmail.com>
2020-11-24 20:54:36 -0800

    commit tree fe5a665613a6490df87b0f6f5223c6d63d478d7e

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,58 @@
+# class Artist
+#     @@all = []
 
+#     attr_accessor :name
+
+#     def initialize(name)
+#         @name = name
+#         save
+#     end
+
+#     def save
+#         @@all << self
+#     end
+
+#     def new_song(name, genre)
+#         Song.new(name, self, genre)
+#     end
+
+#     def songs
+#         Song.all.select do |song|
+#             song.artist == self
+#         end
+#     end
+
+#     def genres
+#         songs.collect(&:genre)
+#     end
+
+#     def self.all
+#         @@all
+#     end
+# end
+class Artist
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def new_song(name, genre)
+      Song.new(name, self, genre)
+    end
+  
+    def songs
+      Song.all.select { |song| song.artist == self }
+    end
+  
+    def genres
+      songs.map(&:genre)
+    end
+  end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,32 @@
+class Genre
+    attr_accessor :name, :artist, :genre
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def artists
+        songs.map(&:artist)
+    end
+
+
+
+    def self.all
+        @@all
+    end
+end
+
+
 
