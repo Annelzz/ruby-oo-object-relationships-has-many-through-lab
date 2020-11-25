c542210fafde17891b479e164ccb20dea55234ee
Annelise <annelzz26@gmail.com>
2020-11-24 20:54:40 -0800

    Done.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,35 @@
+class Doctor
 
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select do |date|
+            date.doctor == self
+        end
+    end
+
+    def patients
+        appointments.map(&:patient)
+    end
+
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,38 @@
+class Patient
+    @@all = []
 
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
+    def doctors
+        Appointment.all.map(&:doctor)
+    end
+            
+
+
+
+
+
+
+end
